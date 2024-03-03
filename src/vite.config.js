5a56fae34d2bf1f496f3f464388542bee2fde629
ooooose <yuuki.oose1990@gmail.com>
2024-03-04 07:55:38 +0900

    Add Vueのインストール

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,16 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        vuetify(),
     ],
     server: {
-        host: true,
-       },
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        }
+    },
 });
