6bb886d406673d268cb8de5a7107aa17dbd21c1e
ooooose <yuuki.oose1990@gmail.com>
2024-03-05 08:13:11 +0900

    Add breeze(Vue)のインストール

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,12 +1,23 @@
 import './bootstrap';
-import { createApp } from "vue";
-import App from "../views/App.vue";
-import { createVuetify } from 'vuetify';
-import router from './router';
-import 'vuetify/dist/vuetify.min.css';
+import '../css/app.css';
 
-const app = createApp(App);
-const vuetify = createVuetify();
-app.use(router);
-app.use(vuetify);
-app.mount('#app');
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
         vue({
@@ -17,12 +16,5 @@ export default defineConfig({
                 },
             },
         }),
-        vuetify(),
     ],
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost'
-        }
-    },
 });
