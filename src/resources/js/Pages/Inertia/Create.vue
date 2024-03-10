26f4f1888b840848a1269d3169ba32188785602a
ooooose <yuuki.oose1990@gmail.com>
2024-03-11 08:02:01 +0900

    Add validationの追加

--- src/resources/js/Pages/Inertia/Create.vue
+++ src/resources/js/Pages/Inertia/Create.vue
@@ -3,6 +3,10 @@
 import { reactive } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
 
+defineProps({
+    errors: Object
+})
+
 const form = reactive({
     title: null,
     content: null
@@ -17,7 +21,9 @@ const submitFunction = () => {
 <template>
     <form @submit.prevent="submitFunction">
         <input type="text" name="title" v-model="form.title" /><br />
-        <input type="text" name="content" v-model="form.content" />
+        <div v-if="errors.title">{{ errors.title }}</div>
+        <input type="text" name="content" v-model="form.content" /><br />
+        <div v-if="errors.content">{{ errors.content }}</div>
         <button>送信</button>
     </form>
 </template>
