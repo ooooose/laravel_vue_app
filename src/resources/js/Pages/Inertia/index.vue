7903d895d919f2970d85e0eb3af4f79033dd66ed
ooooose <yuuki.oose1990@gmail.com>
2024-03-11 08:37:10 +0900

    Fix スタイリングの編集

--- src/resources/js/Pages/Inertia/index.vue
+++ src/resources/js/Pages/Inertia/index.vue
@@ -13,7 +13,7 @@ defineProps({
     </div>
     <ul>
         <li v-for="blog in blogs" :key="blog.id">
-            <Link :href="route('inertia.show', { id: blog.id })">件名: {{ blog.title }}</Link>
+            件名:<Link class="text-blue-400" :href="route('inertia.show', { id: blog.id })">{{ blog.title }}</Link>
             本文: {{ blog.content }}
         </li>
     </ul>
