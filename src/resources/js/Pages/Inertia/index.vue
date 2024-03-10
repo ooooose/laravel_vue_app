9f7baf696bad0ebb8af07eabf9fe12fc72362d8c
ooooose <yuuki.oose1990@gmail.com>
2024-03-11 08:27:46 +0900

    Add flashメッセージの作成

--- src/resources/js/Pages/Inertia/index.vue
+++ src/resources/js/Pages/Inertia/index.vue
@@ -4,6 +4,9 @@
 </script>
 
 <template>
+    <div v-if="$page.props.flash.message" class="bg-blue-300">
+        {{ $page.props.flash.message }}
+    </div>
     InertiaIndexです。<br />
 
     <a href="/">aタグ経由</a>
