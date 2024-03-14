f415fa6f3074749d19bab30f024aebf689b6fc58
ooooose <yuuki.oose1990@gmail.com>
2024-03-14 10:00:51 +0900

    Add Create画面の作成

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -24,7 +24,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create'); 
     }
 
     /**
--- src/resources/js/Pages/Items/Index.vue
+++ src/resources/js/Pages/Items/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '../../Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 
 defineProps({
     items: Array
@@ -22,7 +22,7 @@ defineProps({
                   <section class="text-gray-600 body-font">
                     <div class="container px-5 py-8 mx-auto">
                       <div class="flex pl-4 mb-4 lg:w-2/3 w-full mx-auto">
-                        <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+                        <Link as="button" :href="route('items.create')" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">商品登録</Link>
                       </div>
                       <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                         <table class="table-auto w-full text-left whitespace-no-wrap">
@@ -32,17 +32,20 @@ defineProps({
                               <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">商品名</th>
                               <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">価格</th>
                               <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">ステータス</th>
-                              <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                             </tr>
                           </thead>
                           <tbody>
                             <tr v-for="item in items" :key="item.id">
-                              <td class="px-4 py-3">{{ item.id }}</td>
-                              <td class="px-4 py-3">{{ item.name }}</td>
-                              <td class="px-4 py-3">{{ item.price }}</td>
-                              <td class="px-4 py-3 text-lg text-gray-900">{{ item.is_selling }}</td>
-                              <td class="w-10 text-center">
-                                <input name="plan" type="radio">
+                              <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.id }}</td>
+                              <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.name }}</td>
+                              <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.price }}</td>
+                              <td class="border-b-2 border-gray-200 px-4 py-3 text-lg text-gray-900">
+                                <span v-if="item.is_selling === 1">
+                                    販売中
+                                </span>
+                                <span v-if="item.is_selling === 0">
+                                    停止中
+                                </span>
                               </td>
                             </tr>
                           </tbody>
