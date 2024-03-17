d6728cb945851434e749ab14b4320ee9c4525234
ooooose <yuuki.oose1990@gmail.com>
2024-03-17 17:48:10 +0900

    Add storeメソッドの作成

--- src/resources/js/Pages/Customers/Create.vue
+++ src/resources/js/Pages/Customers/Create.vue
@@ -9,11 +9,13 @@ const form = reactive({
   address: null, birthday: null, gender: null, memo: null
 })
 
-
 const storeCustomer = () => {
   Inertia.post('/customers', form)
 }
 
+defineProps({
+    errors: Object
+})
 
 </script>
 
@@ -32,62 +34,69 @@ const storeCustomer = () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <section class="text-gray-600 body-font relative">
-                          <form @submit.prevent="storeCustomer">
+                            <form @submit.prevent="storeCustomer">
                             <div class="container px-5 py-8 mx-auto">
                             
                             <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                              <div class="flex flex-wrap -m-2">
+                            <div class="flex flex-wrap -m-2">
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="relative">
                                     <label for="name" class="leading-7 text-sm text-gray-600">顧客名</label>
                                     <input type="text" id="name" name="name" v-model="form.name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.name }}</div>
+                                </div>
                                 </div>
 
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="relative">
                                     <label for="kana" class="leading-7 text-sm text-gray-600">顧客名カナ</label>
                                     <input type="text" id="kana" name="kana" v-model="form.kana" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.kana }}</div>
+                                </div>
                                 </div>
 
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="relative">
                                     <label for="tel" class="leading-7 text-sm text-gray-600">電話番号</label>
                                     <input type="tel" id="tel" name="tel" v-model="form.tel" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.tel }}</div>
+                                </div>
                                 </div>
 
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="relative">
                                     <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
                                     <input type="email" id="email" name="email" v-model="form.email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.email }}</div>
+                                </div>
                                 </div>
 
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="relative">
                                     <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
                                     <input type="number" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.postcode }}</div>
+                                </div>
                                 </div>
 
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="relative">
                                     <label for="address" class="leading-7 text-sm text-gray-600">住所</label>
                                     <input type="text" id="address" name="address" v-model="form.address" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.address }}</div>
+                                </div>
                                 </div>
 
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="relative">
                                     <label for="birthday" class="leading-7 text-sm text-gray-600">誕生日</label>
                                     <input type="date" id="birthday" name="birthday" v-model="form.birthday" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.birthday }}</div>
+                                </div>
                                 </div>
 
-                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="p-2 w-full">
+                                <div class="relative">
                                     <label class="leading-7 text-sm text-gray-600">性別</label>
                                     <input type="radio" id="gender0" name="gender" v-model="form.gender" value="0" >
                                     <label for="gender0" class="ml-2 mr-4">男性</label>
@@ -95,25 +104,27 @@ const storeCustomer = () => {
                                     <label for="gender1" class="ml-2 mr-4">女性</label>
                                     <input type="radio" id="gender2" name="gender" v-model="form.gender" value="2" >
                                     <label for="gender2" class="ml-2 mr-4">その他</label>
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.gender }}</div>
+                                </div>
                                 </div>
                                 
                                 <div class="p-2 w-full">
-                                  <div class="relative">
+                                <div class="relative">
                                     <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
                                     <textarea id="memo" name="memo" v-model="form.memo" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                                  </div>
+                                    <div v-if="errors.name" class="text-red-500" >{{ errors.memo }}</div>
+                                </div>
                                 </div>
                                 
                                 
                                 <div class="p-2 w-full">
-                                  <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">顧客登録</button>
+                                    <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">顧客登録</button>
                                 </div>
                                 
-                              </div>
                             </div>
-                          </div>
-                          </form>
+                            </div>
+                        </div>
+                        </form>
                         </section>
                     </div>
                 </div>
