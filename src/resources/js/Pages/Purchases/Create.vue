cb4ffd0bf2b345fdd5f4e46100152706c730b669
ooooose <yuuki.oose1990@gmail.com>
2024-03-21 09:46:35 +0900

    Add selectBoxの修正

--- src/resources/js/Pages/Purchases/Create.vue
+++ src/resources/js/Pages/Purchases/Create.vue
@@ -6,7 +6,7 @@ import { Inertia } from '@inertiajs/inertia'
 import { getToday } from '@/common'
 
 const props = defineProps({
-  // 'customers': Array,
+  'customers': Array,
   'items' : Array
 })
 
@@ -84,13 +84,18 @@ const setCustomerId = id => {
                                   <div class="relative">
                                     <label for="date" class="leading-7 text-sm text-gray-600">日付</label>
                                     <input type="date" id="date" name="date" v-model="form.date" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                  </div>
                                 </div>
-
-                                <div class="p-2 w-full">
-                                  <div class="relative">
+                            </div>
+                            
+                            <div class="p-2 w-full">
+                                <div class="relative">
                                     <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
-                                  </div>
+                                    <select name="customer" v-model="form.customer_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        <option v-for="customer in customers" :value="customer.id" :key="customer.id">
+                                            {{ customer.id }} : {{ customer.name }}
+                                        </option>
+                                    </select>
+                                </div>
                                 </div>
                                 
                               <div class="w-full mt-8 mx-auto overflow-auto">
