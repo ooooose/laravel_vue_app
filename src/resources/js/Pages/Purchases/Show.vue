c0dfc6ae988fce9fc1600a41dbd3381fe7481366
ooooose <yuuki.oose1990@gmail.com>
2024-03-28 10:14:02 +0900

    Add 編集画面の作成

--- src/resources/js/Pages/Purchases/Show.vue
+++ src/resources/js/Pages/Purchases/Show.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '../../Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import { Head, Link } from '@inertiajs/inertia-vue3';
 import { onMounted, reactive, ref, computed } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 import dayjs from 'dayjs';
@@ -109,8 +109,8 @@ onMounted(() => {
                                   </div>
                                 </div>
                                 
-                                <div class="p-2 w-full">
-                                  <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                <div v-if="props.order[0].status == true" class="p-2 w-full">
+                                  <Link as="button" :href="route('purchases.edit', { purchase: props.order[0].id })" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</Link>
                                 </div>
                                 
                               </div>
